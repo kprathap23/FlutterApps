cf747da23dc74bcc776327255b262fa7ae61cd74
PratapKumar <kprathap23@gmail.com>
2024-04-23 16:02:32 +0530

    fixed layout issue

--- fakestore_app/lib/views/ProductDetailsPage.dart
+++ fakestore_app/lib/views/ProductDetailsPage.dart
@@ -16,72 +16,87 @@ class ProductDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Product Detail'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Container(
+        padding: EdgeInsets.all(10),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
-              child: Image.network(
-                product.imageUrl,
-                width: 200,
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Text(
-              product.title,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Price: \$${product.price.toStringAsFixed(2)}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Description:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              product.description,
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Rating:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            RatingBarIndicator(
-              rating: product.rating.rate!,
-              itemBuilder: (context, index) => Icon(
-                Icons.star,
-                color: Colors.amber,
-              ),
-              itemCount: 5,
-              itemSize: 24.0,
-              direction: Axis.horizontal,
-            ),
-            Spacer(),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Add to cart functionality
-                  // Access the ShoppingCartViewModel and call addItemToCart
-                  Provider.of<ShoppingCartViewModel>(context, listen: false)
-                      .addItemToCart(product);
-                  // Show a snackbar to indicate that the product was added to the cart
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('${product.title} added to cart'),
-                      duration: Duration(seconds: 2),
+            Expanded(
+              flex: 1,
+              child: ListView(
+                children: [
+                  Center(
+                    child: Image.network(
+                      product.imageUrl,
+                      width: 200,
+                      height: 200,
+                      fit: BoxFit.cover,
                     ),
-                  );
-                },
-                child: Text('Add to Cart'),
+                  ),
+                  Text(
+                    product.title,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Price: \$${product.price.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Description:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    product.description,
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Rating:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  RatingBarIndicator(
+                    rating: product.rating.rate!,
+                    itemBuilder: (context, index) => Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    itemCount: 5,
+                    itemSize: 24.0,
+                    direction: Axis.horizontal,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[400],
+                    elevation: 0,
+                  ),
+                  onPressed: () {
+                    // Add to cart functionality
+                    // Access the ShoppingCartViewModel and call addItemToCart
+                    Provider.of<ShoppingCartViewModel>(context, listen: false)
+                        .addItemToCart(product);
+                    // Show a snackbar to indicate that the product was added to the cart
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('${product.title} added to cart'),
+                        duration: Duration(seconds: 2),
+                      ),
+                    );
+                  },
+                  child: Text('Add to Cart',style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),),
+                ),
               ),
             ),
           ],
