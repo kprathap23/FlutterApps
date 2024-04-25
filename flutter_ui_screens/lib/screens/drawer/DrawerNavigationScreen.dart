620188bdd960cf2b1768add950b302d0b60b7d97
PratapKumar <kprathap23@gmail.com>
2024-04-25 20:48:03 +0530

    added new NavigationDrawer code to support m3

--- flutter_ui_screens/lib/screens/drawer/DrawerNavigationScreen.dart
+++ flutter_ui_screens/lib/screens/drawer/DrawerNavigationScreen.dart
@@ -18,49 +18,47 @@ class DrawerNavgiationScreen extends StatefulWidget {
 
 class _DrawerNavgiationScreenState extends State<DrawerNavgiationScreen> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  List<Widget> _widgetOptions = <Widget>[];
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      Navigator.pop(context);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Shopping App"),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.notifications),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => NotificationsScreens()),
-              );
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: [
-          HomeScreen(name: "Home"),
-          CategoriesScreen(name: "Categories"),
-          AccountScreen(name: "Account"),
-          CartScreen(name: "Cart"),
-          SettingsScreen(),
-        ][_selectedIndex],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const UserAccountsDrawerHeader(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Shopping App"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.notifications),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const NotificationsScreens()),
+                );
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: [
+            HomeScreen(name: "Home"),
+            CategoriesScreen(name: "Categories"),
+            AccountScreen(name: "Account"),
+            CartScreen(name: "Cart"),
+            SettingsScreen(),
+          ][_selectedIndex],
+        ),
+        drawer: NavigationDrawer(
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: _onItemTapped,
+          children: const [
+            UserAccountsDrawerHeader(
               accountName: Text("Pratap Kumar"),
               accountEmail: Text("kprathap23@gmail.com"),
               decoration: BoxDecoration(
@@ -70,70 +68,124 @@ class _DrawerNavgiationScreenState extends State<DrawerNavgiationScreen> {
                   backgroundImage: NetworkImage(
                       "https://randomuser.me/api/portraits/men/46.jpg")),
             ),
-            // const DrawerHeader(
-            //   decoration: BoxDecoration(
-            //     color: Colors.blue,
+            // Padding(
+            //   padding: EdgeInsets.fromLTRB(28, 16, 16, 10),
+            //   child: Text(
+            //     'Header',
+            //     style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),
             //   ),
-            //   child: Text('Drawer Header'),
             // ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              selected: _selectedIndex == 0,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(0);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+            // Divider(),
+            NavigationDrawerDestination(
+              label: Text("Home"),
+              icon: Icon(Icons.home),
+              selectedIcon: Icon(Icons.home_rounded),
             ),
-            ListTile(
-              leading: const Icon(Icons.category),
-              title: const Text('Categories'),
-              selected: _selectedIndex == 1,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(1);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+            NavigationDrawerDestination(
+              label: Text("Categories"),
+              icon: Icon(Icons.category),
+              selectedIcon: Icon(Icons.category_rounded),
             ),
-            ListTile(
-              leading: const Icon(Icons.person_4),
-              title: const Text('Account'),
-              selected: _selectedIndex == 2,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(2);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+
+            NavigationDrawerDestination(
+              label: Text("Account"),
+              icon: Icon(Icons.person_4),
+              selectedIcon: Icon(Icons.person_4_rounded),
             ),
-            ListTile(
-              leading: const Icon(Icons.shopping_bag),
-              title: const Text('Cart'),
-              selected: _selectedIndex == 3,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(3);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+
+            NavigationDrawerDestination(
+              label: Text("Cart"),
+              icon: Icon(Icons.shopping_bag),
+              selectedIcon: Icon(Icons.shopping_bag_rounded),
             ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              selected: _selectedIndex == 4,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(4);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+
+            NavigationDrawerDestination(
+              label: Text("Settings"),
+              icon: Icon(Icons.settings),
+              selectedIcon: Icon(Icons.settings_rounded),
             ),
           ],
-        ),
-      ),
-    );
+        )
+        // old drawer code
+        // drawer: Drawer(
+        //   child: ListView(
+        //     // Important: Remove any padding from the ListView.
+        //     padding: EdgeInsets.zero,
+        //     children: [
+        //       const UserAccountsDrawerHeader(
+        //         accountName: Text("Pratap Kumar"),
+        //         accountEmail: Text("kprathap23@gmail.com"),
+        //         decoration: BoxDecoration(
+        //           color: Color.fromARGB(255, 244, 141, 110),
+        //         ),
+        //         currentAccountPicture: CircleAvatar(
+        //             backgroundImage: NetworkImage(
+        //                 "https://randomuser.me/api/portraits/men/46.jpg")),
+        //       ),
+        //       // const DrawerHeader(
+        //       //   decoration: BoxDecoration(
+        //       //     color: Colors.blue,
+        //       //   ),
+        //       //   child: Text('Drawer Header'),
+        //       // ),
+        //       ListTile(
+        //         leading: const Icon(Icons.home),
+        //         title: const Text('Home'),
+        //         selected: _selectedIndex == 0,
+        //         onTap: () {
+        //           // Update the state of the app
+        //           _onItemTapped(0);
+        //           // Then close the drawer
+        //           Navigator.pop(context);
+        //         },
+        //       ),
+        //       ListTile(
+        //         leading: const Icon(Icons.category),
+        //         title: const Text('Categories'),
+        //         selected: _selectedIndex == 1,
+        //         onTap: () {
+        //           // Update the state of the app
+        //           _onItemTapped(1);
+        //           // Then close the drawer
+        //           Navigator.pop(context);
+        //         },
+        //       ),
+        //       ListTile(
+        //         leading: const Icon(Icons.person_4),
+        //         title: const Text('Account'),
+        //         selected: _selectedIndex == 2,
+        //         onTap: () {
+        //           // Update the state of the app
+        //           _onItemTapped(2);
+        //           // Then close the drawer
+        //           Navigator.pop(context);
+        //         },
+        //       ),
+        //       ListTile(
+        //         leading: const Icon(Icons.shopping_bag),
+        //         title: const Text('Cart'),
+        //         selected: _selectedIndex == 3,
+        //         onTap: () {
+        //           // Update the state of the app
+        //           _onItemTapped(3);
+        //           // Then close the drawer
+        //           Navigator.pop(context);
+        //         },
+        //       ),
+        //       ListTile(
+        //         leading: const Icon(Icons.settings),
+        //         title: const Text('Settings'),
+        //         selected: _selectedIndex == 4,
+        //         onTap: () {
+        //           // Update the state of the app
+        //           _onItemTapped(4);
+        //           // Then close the drawer
+        //           Navigator.pop(context);
+        //         },
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        );
   }
 }
